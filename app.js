38f5ff9a8dafba1c12786d9074cc121c42b8439d
Karl <khughes.me@gmail.com>
2015-07-31 11:13:25 -0500

    updates

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var http = require('http');
 
 var app = express();
 var port = process.env.PORT || 1337;
@@ -9,11 +10,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // test route
 app.get('/', function (req, res) {
-    inputText = 'isbn:978';
-    inputText = inputText.split(":");
-    var isbn = inputText[1];
-    isbn = isbn.replace(/ /g,'');
-    res.status(200).send(isbn);
+    var isbn = req.query.isbn;
+    var options = getOptions(isbn);
+    getJSON(options, function(statusCode, results) {
+        if (results.total_records === 1) {
+            var result = results.records[0];
+            res.statusCode = statusCode;
+            res.status(200).send(result);
+        } else {
+            res.status(404).end();
+        }
+    });
 });
 
 app.listen(port, function () {
@@ -28,10 +35,58 @@ app.post('/isbn', function (req, res, next) {
         inputText = inputText.split(":");
         var isbn = inputText[1];
         isbn = isbn.replace(/ /g,'');
-        var botPayload = {
-            text : isbn
-        };
-        return res.status(200).json(botPayload);
+        var options = getOptions(isbn);
+        getJSON(options, function(statusCode, results) {
+            if (results.total_records === 1) {
+                var result = results.records[0];
+                var botPayload = {
+                    text : result.isbn13
+                };
+                return res.status(200).json(botPayload);
+            } else {
+                res.status(404).end();
+            }
+        });
     }
     return res.status(404).end();
 });
+
+var getOptions = function(isbn)
+{
+    return {
+        host: 'search.packback.co',
+        path: '/api/search/'+isbn,
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+}
+
+// getJSON:  REST get request returning JSON object(s)
+var getJSON = function(options, onResult)
+{
+    console.log("rest::getJSON");
+
+    var req = http.request(options, function(res)
+    {
+        var output = '';
+        console.log(options.host + ':' + res.statusCode);
+        res.setEncoding('utf8');
+
+        res.on('data', function (chunk) {
+            output += chunk;
+        });
+
+        res.on('end', function() {
+            var obj = JSON.parse(output);
+            onResult(res.statusCode, obj);
+        });
+    });
+
+    req.on('error', function(err) {
+        //res.send('error: ' + err.message);
+    });
+
+    req.end();
+};
