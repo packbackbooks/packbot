95c53862311fe8e29151238da41d8795217e3304
Karl <khughes.me@gmail.com>
2015-07-31 09:10:04 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var port = process.env.PORT || 1337;
+
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// test route
+app.get('/', function (req, res) {
+    res.status(200).send('Hello world!');
+});
+
+app.listen(port, function () {
+  console.log('Listening on port ' + port);
+});
+
+app.post('/hello', function (req, res, next) {
+  var userName = req.body.user_name;
+  var botPayload = {
+    text : 'Hello ' + userName + ', welcome to Packback\'s Slack channel!'
+  };
+  // Loop otherwise..
+  if (userName !== 'slackbot') {
+    return res.status(200).json(botPayload);
+  } else {
+    return res.status(200).end();
+  }
+});
